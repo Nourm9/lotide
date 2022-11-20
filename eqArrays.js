0e261f6f8dfd0c6abcbd1b07e4fab310adcce7c7
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-11-20 21:00:03 +0000

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected};`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// tests
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
