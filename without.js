a11d5d3a93dcc9ca3519b5e248134ed5c1830efc
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-11-21 01:08:56 +0000

    add: without.js

---
+++ without.js
@@ -0,0 +1,35 @@
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    }
+  }
+  console.log(`✅✅✅  Assertion Passed: ${array1}  ===  ${array2}`);
+}
+
+function without(source, itemsToRemove) {
+  let newArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArr.push(source[i]);
+    }
+  }
+  return newArr;
+}
+
+// tests
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
